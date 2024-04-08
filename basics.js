8d38db9904b14d4cd92df10a2a4bb85f05c0adfd
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2024-04-08 21:52:09 +0530

    Comparision

---
+++ basics.js
@@ -0,0 +1,72 @@
+// Data Types: 
+
+// 1. Number 
+// 2. String
+// 3. Boolean
+
+a = 100.67
+// console.log(a, typeof(a))
+// b = "Hello"
+// c = true
+a = "Hello"
+
+// console.log(a, typeof(a)) // 100
+
+
+
+// if else: 
+
+// arithmetic operators: +, -, *, /, %, **(power)
+// comparison operators: >, <, >=, <=, ==, ===, !=, !== 
+// logical operators: &&, ||, !
+
+// if(){
+
+// }
+// else if(){
+
+// }
+
+// else{
+
+// }
+
+
+// a = 10 
+// b = 3 
+// console.log(a/b)
+
+// == , ===
+
+// a = 10  // number
+// b = "10" // string
+
+// console.log(a == b) // == only checks the value 
+
+// console.log(a === b) // === checks the value and the type
+
+
+
+// fun time: 
+
+// a = -10 
+// b = 3 
+
+// console.log(a%b)
+
+
+// loops: 
+// str = ""
+// for(i = 1; i<=5; i=i+1){
+//     str = str + i+ " "
+// }
+// console.log(str)
+
+// i = 1
+// while(i<=5){
+//     console.log(i)
+//     i = i+1
+// }
+
+// console.log("Hello World")
+// console.log("Hello World")
---
+++ scope.js
@@ -0,0 +1,30 @@
+
+// console.log(a)
+
+// a = 100 
+
+// console.log(a)
+
+// let
+
+
+// let a = 100
+// c = 500
+
+// for(i = 1; i<=1; i=i+1){
+//     a = 50
+//    let b = 60
+// }
+
+// // console.log(a)
+// console.log(b)
+
+
+let a = 50 
+c = 300 
+
+if(10>5){
+    let b = 60
+}
+
+console.log(a+b+c)
